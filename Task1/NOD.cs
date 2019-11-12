99151e67e89fd3daecf02b126a4de537d7f681fa
Gordey Medvedskiy <medvedsky.gordei@yandex.ru>
2019-11-12 20:18:33 +0300

    Начал работу

---
+++ Task1/NOD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class NOD
+    {
+        public int number1, number2;
+
+        public NOD(int number1, int number2)
+        {
+            this.number1 = number1;
+            this.number2 = number2;
+        }
+
+        public int EuclidAlg(int number1, int number2)
+        {
+            while ((number1 != 0) && (number2 != 0))
+            {
+                if (number1 > number2)
+                    number1 -= number2;
+                else
+                    number2 -= number1;
+            }
+
+            return Math.Max(number1, number2);
+        }
+    }
+}
