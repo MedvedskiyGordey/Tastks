fbc607d6b4f13ae7536c94e156d733e425aaa0b2
Gordey Medvedskiy <medvedsky.gordei@yandex.ru>
2019-12-10 12:44:38 +0300

    created interfaces

---
+++ Task3/Task3/FilmFigure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    interface FilmFigure : Ifigures
+    {
+        Color GetColor();
+        void Paint(Color color);
+        bool Painted();
+    }
+}
---
+++ Task3/Task3/Ifigures.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public enum Color
+    {
+        None, Red, Green, Blue, White, Black
+    }
+    public interface Ifigures
+    {
+        double Square { get; }
+        double Perimetr { get; }
+        string GetMaterial();
+        string GetTypeFigure();
+    }
+}
