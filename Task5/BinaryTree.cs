89df8b7ad774de2a7cf04f38b734248b1f8e65e8
Gordey Medvedskiy <medvedsky.gordei@yandex.ru>
2019-12-29 14:37:13 +0300

    Created BinaryTree and Node classes

---
+++ Task5/BinaryTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Task5
+{
+    [DataContract(IsReference = true)]
+    public class BinaryTree<T> where T : IComparable
+    {
+
+    }
+}
---
+++ Task5/Node.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Task5
+{
+    public enum Side
+    {
+        Left,
+        Right
+    }
+
+    [DataContract(IsReference = true)]
+    public class Node<T> where T : IComparable
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        [DataMember]
+        public T Data { get; set; }
+
+        [DataMember]
+        public Node<T> LeftBranch { get; set; }
+
+        [DataMember]
+        public Node<T> RightBranch { get; set; }
+
+        [DataMember]
+        public Node<T> ParentBranch { get; set; }
+
+        [DataMember]
+        public Side? BranchSide
+        {
+            get
+            {
+                if (ParentBranch == null)
+                {
+                    return null;
+                }
+                else if (ParentBranch.LeftBranch == this)
+                {
+                    return Side.Left;
+                }
+                else
+                {
+                    return Side.Right;
+                }
+            }
+            private set
+            {
+
+            }
+        }
+    }
+}
